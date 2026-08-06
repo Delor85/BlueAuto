107b749e605a171f1efafd64cce0e60ae785c8ae
Delor85 <mbolodelorpro@gmail.com>
2026-08-06 12:33:58 +0100

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,37 @@
+package com.profitloop.blueclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        
+        webView.setWebViewClient(new WebViewClient());
+        
+        // Point d'entrée de l'application pour la connexion des agents (DAE, DSM, PoS)
+        webView.loadUrl("https://magicservice-blue.gt.tc/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
