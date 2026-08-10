7d4d81530c8e426d8cc3f9fd460937bb383e30d1
Delor85 <mbolodelorpro@gmail.com>
2026-08-10 19:40:21 +0100

    fix(android): restore URI import after overlay removal

--- app/src/main/java/com/profitloop/blueauto/MainActivity.java
+++ app/src/main/java/com/profitloop/blueauto/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
